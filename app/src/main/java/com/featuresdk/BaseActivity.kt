623b082a349de5708c7741bdb5cb36276642b5b5
Piotr Iwaniec <piotr.iwaniec@miquido.com>
2019-10-08 08:19:30 +0200

    add base activity with overriding base context

---
+++ app/src/main/java/com/featuresdk/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.featuresdk
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.simplefeature.SimpleFeatureSdk
+
+open class BaseActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        val languageProvider = SimpleFeatureSdk.instance.sdkConfiguration.languageContextProvider
+        super.attachBaseContext(newBase.let(languageProvider))
+    }
+}
